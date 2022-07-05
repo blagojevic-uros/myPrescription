2e8f3a2f0624f571c0bdb40f8b93f488535ceeee
Uros Blagojevic <blagojevic.prog@gmail.com>
2022-07-05 15:55:03 +0200

    Unauth User Frontend

---
+++ myPrescriptionFrontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DrugListComponent } from './drug-list/drug-list.component';
+import { DrugComponent } from './drug/drug.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DrugListComponent,
+    DrugComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
