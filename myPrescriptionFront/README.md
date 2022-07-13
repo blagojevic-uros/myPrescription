0e3233cf6436f7c1734f83e485ab4369b8a9a5bc
Uros Blagojevic <blagojevic.prog@gmail.com>
2022-07-13 11:49:33 +0200

    Refactor

--- myPrescriptionFront/README.md
+++ myPrescriptionFront/README.md
@@ -1,4 +1,4 @@
-# MyPrescriptionFrontend
+# MyPrescriptionFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- myPrescriptionFront/src/app/app.module.ts
+++ myPrescriptionFront/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { DrugListComponent } from './drug-list/drug-list.component';
-import { DrugComponent } from './drug/drug.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DrugListComponent,
-    DrugComponent,
-    LoginComponent,
-    RegistrationComponent
   ],
   imports: [
     BrowserModule,
